f2894fa43226adb1b443cb79971fd87a3f1eda6a
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-15 16:30:44 -0300

    Update preencheFormTJMAThree.py

--- preencheFormTJMAThree.py
+++ preencheFormTJMAThree.py
@@ -83,10 +83,6 @@ def zeraWidget(opt):
                     st.session_state[key] = []
                 elif k in [7, 8]:
                     st.session_state[key] = False
-                elif k in [13]:
-                    st.session_state[key] = 0
-                elif k in [14]:
-                    st.session_state[key] = 2
                 else:
                     st.session_state[key] = ''        
     else:
@@ -96,10 +92,6 @@ def zeraWidget(opt):
                 st.session_state[key] = []
             elif k in [7, 8]:
                 st.session_state[key] = False
-            elif k in [13]:
-                st.session_state[key] = 0
-            elif k in [14]:
-                st.session_state[key] = 2
             else:
                 st.session_state[key] = ''    
 
@@ -160,8 +152,8 @@ def main():
            cod, codV = st.columns([6.2, 2])
            cpf = cod.text_input('CPF', key=allKeys[11], value=st.session_state[allKeys[11]])
            cpfV = codV.text_input('Verificador', key=allKeys[12], value=st.session_state[allKeys[12]])
-           edital = st.selectbox('Edital Conjunto TJMA/PGE-MA nº___/2025', optionsEdit, key=allKeys[13], index=0)
-           rodada = st.selectbox('Edital da Rodada de Chamamento n.°', optionsRod, key=allKeys[14], index=2)
+           edital = st.selectbox('Edital Conjunto TJMA/PGE-MA nº___/2025', optionsEdit, key=allKeys[13])
+           rodada = st.selectbox('Edital da Rodada de Chamamento n.°', optionsRod, key=allKeys[14])
     if modelOne and modelTwo:
         del st.session_state[allKeys[7]]
         del st.session_state[allKeys[8]]
