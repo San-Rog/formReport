90d683f648d92ce1d3d7f7d9f01b1af6234a9874
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-15 16:03:19 -0300

    Update preencheFormTJMAThree.py

--- preencheFormTJMAThree.py
+++ preencheFormTJMAThree.py
@@ -97,9 +97,9 @@ def zeraWidget(opt):
             elif k in [7, 8]:
                 st.session_state[key] = False
             elif k == 13:
-                st.session_state[key] = 0
+                st.session_state[key] = '0'
             elif k == 14:
-                st.session_state[key] = 2
+                st.session_state[key] = '2'
             else:
                 st.session_state[key] = ''    
 
