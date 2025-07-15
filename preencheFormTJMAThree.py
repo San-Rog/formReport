cc861e90169869279767df967e9aa6cb75081ce2
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-15 15:11:27 -0300

    Update preencheFormTJMAThree.py

--- preencheFormTJMAThree.py
+++ preencheFormTJMAThree.py
@@ -79,17 +79,11 @@ def zeraWidget(opt):
     if opt == 0:
         for k, key in enumerate(allKeys):
             if key not in st.session_state:
-                if k == 3:
-                    st.session_state[key] = list()
-                else:
-                    st.session_state[key] = ''        
+                st.session_state[key] = ''        
     else:
         for k, key in enumerate(allKeys):
             del st.session_state[key] 
-            if k == 3:
-                 st.session_state[key] = list()
-            else:
-                st.session_state[key] = '' 
+            st.session_state[key] = '' 
 
 def ckeckPlaces(): 
     placeVoid = False
