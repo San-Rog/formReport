93217149dba95c552f085fcd9607712a4d619d89
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-15 14:40:53 -0300

    Update preencheFormTJMAThree.py

--- preencheFormTJMAThree.py
+++ preencheFormTJMAThree.py
@@ -107,8 +107,6 @@ def main():
     global formPdf, precat, requer, proc, obj, modelOne, modelTwo, bank, agency, verify, cpf, cpfV, edital, rodada, count, countV
     global day, mont, year, keysCount
     global optionsCount, optionsCred
-    global allKeys 
-    allKeys = ['prc', 'req']
     optionsEdit = [str(n) for n in range(1, 5)]
     optionsRod = [str(n) for n in range(1, 5)]
     keysCount = ['um', 'dois']
@@ -168,6 +166,8 @@ def main():
                 zeraWidget(1)
                 
 if __name__ == '__main__':
+    global allKeys 
+    allKeys = ['prc', 'req']
     zeraWidget(0)
     st.set_page_config(layout="wide")
     main()
