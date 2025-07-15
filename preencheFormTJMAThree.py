b6c33c1ed4f7a7cdfa23f037816b43554166ee48
San-Rog <118987989+San-Rog@users.noreply.github.com>
2025-07-15 15:33:47 -0300

    Update preencheFormTJMAThree.py

--- preencheFormTJMAThree.py
+++ preencheFormTJMAThree.py
@@ -77,14 +77,19 @@ def message(head, text):
 
 def zeraWidget(opt):
     if opt == 0:
-        for key in allKeys:
+        for k, key in enumerate(allKeys):
             if key not in st.session_state:
-                st.session_state[key] = ''
-        
+                if k == 3:
+                    st.session_state[key] = [optionsCred[0]]
+                else:
+                    st.session_state[key] = ''        
     else:
-        for key in allKeys:
+        for k, key in enumerate(allKeys):
             del st.session_state[key] 
-            st.session_state[key] = ''
+            if k == 3:
+                 st.session_state[key] = [optionsCred[0]]
+            else:
+                st.session_state[key] = ''
 
 def ckeckPlaces(): 
     placeVoid = False
@@ -108,7 +113,7 @@ def main():
     global day, mont, year, keysCount
     global optionsCount, optionsCred
     global allKeys 
-    allKeys = ['prc', 'req', 'process', 'credyte']
+    allKeys = ['prc', 'req', 'process', 'credyt']
     optionsEdit = [str(n) for n in range(1, 5)]
     optionsRod = [str(n) for n in range(1, 5)]
     keysCount = ['um', 'dois']
@@ -125,7 +130,7 @@ def main():
             precat = st.text_input('Precatório n°', key=allKeys[0], value=st.session_state[allKeys[0]])
             requer = st.text_input('Requerente(s)', key=allKeys[1], value=st.session_state[allKeys[1]])
             proc = st.text_input('Referência (Ação Originária/Execução)', key=allKeys[2], value=st.session_state[allKeys[2]])
-            obj = st.multiselect('Crédito Negociado', optionsCred, default=[optionsCred[0]], key=allKeys[3])
+            obj = st.multiselect('Crédito Negociado', optionsCred, default=st.session_state[allKeys[3]], key=allKeys[3])
         with colBank:
             bank = st.text_input('Banco', key='bank')
             colAgency, colDigit = st.columns([6.2, 2])
